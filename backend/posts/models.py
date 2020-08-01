0e628f0fb35d5e4a2bcb9c18598cbe0adb31997e
unknown <jeweldatta2@gmail.com>
2020-08-01 14:12:29 +0600

    add posts model

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    caption = models.TextField(max_length=255)
+    creator = models.ForeignKey(
+        get_user_model(), null=True, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.caption} is created by {self.creator.username}"
+
+
+class Like(models.Model):
+    liked_by = models.ForeignKey(
+        get_user_model(), null=True, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, null=True, related_name='likes', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.liked_by.username}  liked {self.post.caption}"
